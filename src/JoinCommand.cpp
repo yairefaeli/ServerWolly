7a0cf50ac080e6b784c0b84105c16a9761b695f1
yair <yairefaeli324@gmail.com>
2018-01-02 15:24:56 +0200

    e

--- src/JoinCommand.cpp
+++ src/JoinCommand.cpp
@@ -10,8 +10,9 @@ this->threadMap=threadMap;
 }
 
 string joinCommand::execute(vector<string> args) {
-    if(threadMap->count(*args.at(1)) ==1){
-        string str = *args.at(1);
+    if (threadMap->count(args.at(1)) == 1) {
+
+        string str = args.at(1);
         (*(this->threadMap))[str]->join(this->firstClientSocket);
     }
 }
